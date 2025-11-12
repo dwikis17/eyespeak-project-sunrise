d81e85c2faa261c665e755e30c1d394572e2ce46
dwiki <dwikis17@gmail.com>
2025-11-12 15:31:13 +0700

    add settings

---
+++ eyespeak/Sources/Features/Settings/Components/ActionCard.swift
@@ -0,0 +1,18 @@
+//
+//  ActionCard.swift
+//  eyespeak
+//
+//  Created by Dwiki on 12/11/25.
+//
+
+import SwiftUI
+
+struct ActionCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActionCard()
+}
---
+++ eyespeak/Sources/Features/Settings/Components/ActionListSelection.swift
@@ -0,0 +1,18 @@
+//
+//  ActionListSelection.swift
+//  eyespeak
+//
+//  Created by Dwiki on 12/11/25.
+//
+
+import SwiftUI
+
+struct ActionListSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActionListSelection()
+}
